c8cb51eeade09c252611a02526a2e8d7613432d6
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-03-22 10:57:43 +0000

    html terminer

--- js/modal.js
+++ js/modal.js
@@ -8,11 +8,54 @@ modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
 
 function toggleModal(){
     modalContainer.classList.toggle("active");
+    console.log("hello")
 }
-btn.addEventListener("click", function (e){
-    inpt.value = ""
+
+
+// btn.addEventListener("click", function (e){
+//     inpt.value = ""
     // location.reload()
-})
+// })
+
 
 let textContainer = document.querySelector(".text-container")
-let btnClic = document.querySelector(".btn-clic")
+let btnClic = document.querySelectorAll(".btn-clic")
+
+btnClic.forEach(clic => clic.addEventListener("click", modalClick))
+function modalClick(){
+    textContainer.classList.toggle("active")
+}
+
+
+let orangeContainer = document.querySelector(".orange_container")
+let btnOrange = document.querySelectorAll(".btn-orange")
+
+btnOrange.forEach(orang => orang.addEventListener("click", orangeClick))
+function orangeClick(){
+    orangeContainer.classList.toggle("active")
+}
+
+let mtnContainer = document.querySelector(".mtn_container")
+let btnmtn = document.querySelectorAll(".btn-mtn")
+
+btnmtn.forEach(mtn => mtn.addEventListener("click", mtnClick))
+function mtnClick(){
+    mtnContainer.classList.toggle("active")
+}
+
+let waveContainer = document.querySelector(".wave_container")
+let btnwave = document.querySelectorAll(".btn-wave")
+
+btnwave.forEach(wave => wave.addEventListener("click", waveClick))
+function waveClick(){
+    waveContainer.classList.toggle("active")
+}
+
+
+let moovContainer = document.querySelector(".moov_container")
+let btnmoov = document.querySelectorAll(".btn-moov")
+
+btnmoov.forEach(moov => moov.addEventListener("click", moovClick))
+function moovClick(){
+    moovContainer.classList.toggle("active")
+}
--- js/travailleur.js
+++ js/travailleur.js
@@ -6,44 +6,44 @@ let var3 = document.querySelector(".statut")
 let var4 = document.querySelector(".contact")
 
 
-btn.addEventListener("click", function(e){
+// btn.addEventListener("click", function(e){
 
-    // creation de nouvelle div
+//     // creation de nouvelle div
 
 
-    var div = document.createElement('div')
-    div.style.height=" 50px"
-    div.style.width="95.5%"
-    div.style.border="1px solid #0D5447"
-    div.style.margin="0 0 0 2.5%"
-    div.style.display="flex"
-    div.style.padding= "1% 8% 0"
-    div.style.justifyContent= "space-between"
+//     var div = document.createElement('div')
+//     div.style.height=" 50px"
+//     div.style.width="95.5%"
+//     div.style.border="1px solid #0D5447"
+//     div.style.margin="0 0 0 2.5%"
+//     div.style.display="flex"
+//     div.style.padding= "1% 8% 0"
+//     div.style.justifyContent= "space-between"
  
 
-    // creation de nouveau elements 
+//     // creation de nouveau elements 
 
 
-    let nom = document.createElement("label")
-    let surname = document.createElement("label")
-    let stauts = document.createElement("label")
-    let call = document.createElement("label")
+//     let nom = document.createElement("label")
+//     let surname = document.createElement("label")
+//     let stauts = document.createElement("label")
+//     let call = document.createElement("label")
 
 
 
-    // ajout des valeurs au differents e
+//     // ajout des valeurs au differents e
 
-    nom.textContent = var1.value
-    surname.textContent = var2.value
-    stauts.textContent = var3.value
-    call.textContent = var4.value
+//     nom.textContent = var1.value
+//     surname.textContent = var2.value
+//     stauts.textContent = var3.value
+//     call.textContent = var4.value
 
 
-    tab.appendChild(div)
-    div.appendChild(nom)
-    div.appendChild(surname)
-    div.appendChild(stauts)
-    div.appendChild(call)
+//     tab.appendChild(div)
+//     div.appendChild(nom)
+//     div.appendChild(surname)
+//     div.appendChild(stauts)
+//     div.appendChild(call)
 
-    return false
-})
+//     return false
+// })
