633cbc2bddce1f88273a31bf83ee00f74367980c
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-03-24 11:53:57 +0000

    sve

--- exo.js
+++ exo.js
@@ -30,42 +30,42 @@ function user_save(){
 
     }
     const line = document.createElement('div');
-        line.className='attributs';
-        line.id='attribut'+objEmpl.id.replace('Tr','');
-        const idt = document.createElement('p')
-        idt.id="ids"+(tabJ.length+1);
-        idt.textContent=objEmpl.id
-        line.append(idt)
-        const user = document.createElement('p')
-        user.id='users'+(tabJ.length+1);
-        user.textContent=objEmpl.nom
-        line.append(user)
-        const contacts = document.createElement('p')
-        contacts.id='contacts'+(tabJ.length+1);
-        contacts.textContent=objEmpl.contact
-        line.append(contacts)
-        const count = document.createElement('p')
-        count.id='counts'+(tabJ.length+1);
-        count.textContent=objEmpl.compte
-        line.append(count)
-        const action = document.createElement('p')
-        action.id='action'+(tabJ.length+1);
-        line.append(action)
-        const option = document.createElement('div')
-        option.className='option';
-        option.id='option'+(tabJ.length+1);
-        action.append(option)
-        const left = document.createElement('div')
-        left.className='left';
-        left.textContent='modifier'
-        left.id='left'+(tabJ.length+1);
-        option.append(left)
-        const right = document.createElement('div')
-        right.className='right';
-        right.textContent='supprimer';
-        right.id='right'+(tabJ.length+1);
-        action.append(right);
-        document.getElementById("array").append(line)
+    line.className='attributs';
+    line.id='attribut'+objEmpl.id.replace('Tr','');
+    const idt = document.createElement('p')
+    idt.id="ids"+(tabJ.length+1);
+    idt.textContent=objEmpl.id
+    line.append(idt)
+    const user = document.createElement('p')
+    user.id='users'+(tabJ.length+1);
+    user.textContent=objEmpl.nom
+    line.append(user)
+    const contacts = document.createElement('p')
+    contacts.id='contacts'+(tabJ.length+1);
+    contacts.textContent=objEmpl.contact
+    line.append(contacts)
+    const count = document.createElement('p')
+    count.id='counts'+(tabJ.length+1);
+    count.textContent=objEmpl.compte
+    line.append(count)
+    const action = document.createElement('p')
+    action.id='action'+(tabJ.length+1);
+    line.append(action)
+    const option = document.createElement('div')
+    option.className='option';
+    option.id='option'+(tabJ.length+1);
+    action.append(option)
+    const left = document.createElement('div')
+    left.className='left';
+    left.textContent='modifier'
+    left.id='left'+(tabJ.length+1);
+    option.append(left)
+    const right = document.createElement('div')
+    right.className='right';
+    right.textContent='supprimer';
+    right.id='right'+(tabJ.length+1);
+    action.append(right);
+    document.getElementById("array").append(line)
 
 }
 
--- js/travailleur.js
+++ js/travailleur.js
@@ -1,21 +1,27 @@
-let btn = document.querySelector(".btn")
 
 
-btn.addEventListener("click", user-save)
+let soumettre = document.getElementById("submit")
+
+soumettre.addEventListener("click", user_save)
 
 function user_save(){
-    const nom = document.getElementById('nom')
-    const prenom = document.getElementById("prenom")
-    const profession = document.getElementById('profession')
-    const contact = document.getElementById('contact')
+    const addNom = document.getElementById('nom')
+    const addPrenom = document.getElementById("prenom")
+    const addProfession = document.getElementById('profession')
+    const addContact = document.getElementById('contact')
+alert("bbbbb")
+    console.log(addNom.value)
+    console.log(addPrenom.value);
+    console.log(addProfession.value)
+    console.log(addContact.value);
 
     const tab = [];
     const objEmpl = {
         id : "",
-        nom : nom.value,
-        prenom : prenom.value,
-        profession : profession.value,
-        contact : contact.value
+        addNom : addNom.value,
+        addPrenom : addPrenom.value,
+        addProfession : addProfession.value,
+        addContact : addContact.value
     }
 
     const dataEmpl = localStorage.getItem('employes');
@@ -33,32 +39,59 @@ function user_save(){
 
     const line = document.createElement("tr");
     line.className = 'attributs';
-    line.id="attributs"+objEmpl.id.replace('Tr', '');
-
+    line.id="attribut"+objEmpl.id.replace('Tr', '');
     const idt = document.createElement('td');
     idt.id='ids'+(tabJ.length+1);
     idt.textContent=objEmpl.id;
     line.append(idt)
-
     const user = document.createElement('td')
-    user.id='users'+(tabJ.length+1);
-    user.textContent=objEmpl.nom
+    user.id='nom'+(tabJ.length+1);
+    user.textContent=objEmpl.addNom
     line.append(user)
-
     const surname = document.createElement('td')
-    surname.id='surname'+(tabJ.length+1);
-    surname.textContent=objEmpl.prenom
+    surname.id='prenom'+(tabJ.length+1);
+    surname.textContent=objEmpl.addPrenom
     line.append(surname)
-
     const job = document.createElement('td')
-    job.id='surname'+(tabJ.length+1);
-    job.textContent=objEmpl.profession
+    job.id='profession'+(tabJ.length+1);
+    job.textContent=objEmpl.addProfession
     line.append(job)
-
     const contacts = document.createElement('td')
-    contacts.id='contacts'+(tabJ.length+1);
-    contacts.textContent=objEmpl.contact
+    contacts.id='contact'+(tabJ.length+1);
+    contacts.textContent=objEmpl.addContact
     line.append(contacts)
-    
-    const modifier = document.createElement("")
 }
+
+const selectUer =JSON.parse(localStorage.getItem('employes'))
+function affiche(params){
+    if((params.length != 0)){
+        params.forEach(element => {
+            const line = document.createElement("tr");
+            line.className = 'attributs';
+            line.id="attributs"+objEmpl.id.replace('Tr', '');
+            const idt = document.createElement('td');
+            idt.id='ids'+(tabJ.length+1);
+            idt.textContent=objEmpl.id;
+            line.append(idt)
+            const user = document.createElement('td')
+            user.id='nom'+(tabJ.length+1);
+            user.textContent=objEmpl.addNom
+            line.append(user)        
+            const surname = document.createElement('td')
+            surname.id='prenom'+(tabJ.length+1);
+            surname.textContent=objEmpl.addPrenom
+            line.append(surname)  
+            const job = document.createElement('td')
+            job.id='profession'+(tabJ.length+1);
+            job.textContent=objEmpl.addProfession
+            line.append(job)       
+            const contacts = document.createElement('td')
+            contacts.id='contact'+(tabJ.length+1);
+            contacts.textContent=objEmpl.addContact
+            line.append(contacts)
+            document.getElementById("attribut").append(line)
+
+        });
+    }
+}
+affiche(selectUer)
