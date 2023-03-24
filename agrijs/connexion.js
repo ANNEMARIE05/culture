c078a04582ed493b04a20c0ab9b206bde6978e54
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-03-24 14:26:17 +0000

    conecter

--- agrijs/connexion.js
+++ agrijs/connexion.js
@@ -18,13 +18,12 @@ btn.addEventListener("click" , function(e){
    let mail = email.value;
    let motPasse = passe.value;
    if(admin.email === mail && admin.password === motPasse){
-    window.location.pathname="../agrihtml/accueil.html";
+    window.location.pathname="agrihtml/accueil.html";
     localStorage.setItem("admin",JSON.stringify(admin))
    }else if(superAdmin.email === mail && superAdmin.password === motPasse){
-    window.location.pathname="../agriadmin/accueilAdmin.html";
+    window.location.pathname="agriadmin/accueilAdmin.html";
     localStorage.setItem("superAdmin",JSON.stringify(superAdmin))
    }else{
-    mail.
     alert("Les champs que vous avez remplir sont incorrects . Vous n'êtes pas autoriser a acceder a la plateforme ")
    }
 
